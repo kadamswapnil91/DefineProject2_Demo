324ddf0e3b2e891aee7ddc03e2e047b2d404c344
Swapnil <kadamswapnil91@gmail.com>
2020-06-26 13:21:44 +0530

    Initial Commit

---
+++ Assigment 2 Alamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assigment 2 Alamofire
+//
+//  Created by Swapnil Kadam on 26/06/20.
+//  Copyright © 2020 Swapnil Kadam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
